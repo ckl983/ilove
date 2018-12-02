d073d9b0b4e80029a777edd382f793df80cba77f
ckl983 <983744115@qq.com>
2018-12-02 23:18:11 +0800

    Tp5.2

---
+++ application/index/controller/Index.php
@@ -0,0 +1,10 @@
+<?php
+namespace app\index\controller;
+
+class Index
+{
+    public function index()
+    {
+        echo "<h1>我家的小宝贝in_0</h1>";
+    }
+}
--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: ckl
- * Date: 2018/11/30
- * Time: 17:15
- */
-phpinfo();
